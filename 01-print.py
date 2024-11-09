b23041688cb562a540d1a377c60f48cbd1a169a2
Jared Marulli <jemarulli@gmail.com>
2024-11-09 00:17:19 -0500

    added input

---
+++ 01-print.py
@@ -0,0 +1,4 @@
+print("Jared Marulli")
+print("0----")
+print(" ||||")
+print("*"*10)
---
+++ 02-variables.py
@@ -0,0 +1,29 @@
+price = 10
+print(price)
+
+price = 20
+print(price)
+
+rating = 4.9
+print(price*rating)
+
+name = "jared"
+
+is_published = False # True or False must be capitalized
+
+
+## TEST
+## We checked in a patient named John Smith
+## He is 20 years old and a new patient
+
+newname = "John Smith"
+age = 20
+is_new_patient = True
+
+print(newname)
+
+if is_new_patient:
+    print(newname + " is a new patient")
+else:
+    print(newname + " is a returning patient")
+
